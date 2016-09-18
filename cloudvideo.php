54fd219fccdcefcf9b730e56d24d8b71405ef5bc
dragonzx <DragonZX@yandex.ru>
2016-09-18 21:54:15 +0300

    init functions

--- cloudvideo.php
+++ cloudvideo.php
@@ -85,11 +85,9 @@ class plgContentcloudvideo extends JPlugin
 
         $width = $params->get('width', 432);
         $height = $params->get('height', 243);
-        $confidence = $params->get('confidence', 0);
         $fullscreen = $params->get('fullscreen', 1);
 
         #Logic
-        if($confidence) $url='www.youtube-nocookie.com'; else $url='www.youtube.com';
         if($fullscreen) $fscr=' allowfullscreen'; else $fscr='';
 
         return '<iframe width="'.$width.'" height="'.$height.'" src="https://'.$url.'/embed/videoseries?list='.$vCode.'" frameborder="0" '.$fscr.'></iframe>';
@@ -102,12 +100,13 @@ class plgContentcloudvideo extends JPlugin
         $width = $params->get('width', 432);
         $height = $params->get('height', 243);
         $fullscreen = $params->get('fullscreen', 1);
+        $startin = 0;
+
 
         #Logic
-        if($confidence) $url='www.youtube-nocookie.com'; else $url='www.youtube.com';
-        if($fullscreen) $fscr=' allowfullscreen'; else $fscr='';
+        if($fullscreen) $fscr=' webkitallowfullscreen mozallowfullscreen allowfullscreen'; else $fscr='';
 
-        return '<iframe width="'.$width.'" height="'.$height.'" src="https://rutube.ru/play/embed/'.$vCode.'" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>';
+        return '<iframe width="'.$width.'" height="'.$height.'" src="https://rutube.ru/play/embed/'.$vCode.'" frameborder="0" '.$fscr.'></iframe>';
     }
 
     function vimeoVideo($vCode)
@@ -116,14 +115,12 @@ class plgContentcloudvideo extends JPlugin
 
         $width = $params->get('width', 432);
         $height = $params->get('height', 243);
-        $confidence = $params->get('confidence', 0);
         $fullscreen = $params->get('fullscreen', 1);
 
         #Logic
-        if($confidence) $url='www.youtube-nocookie.com'; else $url='www.youtube.com';
-        if($fullscreen) $fscr=' allowfullscreen'; else $fscr='';
+        if($fullscreen) $fscr=' webkitallowfullscreen mozallowfullscreen allowfullscreen'; else $fscr='';
 
-        return '<iframe src="https://player.vimeo.com/video/'.$vCode.'?title=0&byline=0&portrait=0&badge=0" width="'.$width.'" height="'.$height.'" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+        return '<iframe src="https://player.vimeo.com/video/'.$vCode.'?title=0&byline=0&portrait=0&badge=0" width="'.$width.'" height="'.$height.'" frameborder="0" '.$fscr.'></iframe>';
     }
 
     function dmVideo($vCode)
@@ -132,14 +129,12 @@ class plgContentcloudvideo extends JPlugin
 
         $width = $params->get('width', 432);
         $height = $params->get('height', 243);
-        $confidence = $params->get('confidence', 0);
         $fullscreen = $params->get('fullscreen', 1);
 
         #Logic
-        if($confidence) $url='www.youtube-nocookie.com'; else $url='www.youtube.com';
         if($fullscreen) $fscr=' allowfullscreen'; else $fscr='';
 
-        return '<iframe frameborder="0" width="480" height="270" src="//www.dailymotion.com/embed/video/x38nftj?autoPlay=1&start=90" allowfullscreen></iframe><iframe frameborder="0" width="480" height="270" src="//www.dailymotion.com/embed/video/x38nftj?autoPlay=1&start=90" allowfullscreen></iframe>';
+        return '<iframe frameborder="0" width="'.$width.'" height="'.$height.'" src="//www.dailymotion.com/embed/video/'.$vCode.'?autoPlay=1&start=90" '.$fscr.'></iframe><iframe frameborder="0" width="480" height="270" src="//www.dailymotion.com/embed/video/x38nftj?autoPlay=1&start=90" allowfullscreen></iframe>';
     }
 
     function flVideo($vCode)
@@ -148,14 +143,17 @@ class plgContentcloudvideo extends JPlugin
 
         $width = $params->get('width', 432);
         $height = $params->get('height', 243);
-        $confidence = $params->get('confidence', 0);
-        $fullscreen = $params->get('fullscreen', 1);
+        $flheader = $params->get('flheader', 0);
+        $flfooter = $params->get('flfooter', 0);
+        $flcontext = $params->get('flfooter', 0);
 
         #Logic
-        if($confidence) $url='www.youtube-nocookie.com'; else $url='www.youtube.com';
-        if($fullscreen) $fscr=' allowfullscreen'; else $fscr='';
+        $flvars='';
+        if($flheader) $flvars=$flvars.'data-header="true"';
+        if($flfooter) $flvars=$flvars.'data-footer="true"';
+        if($flcontext) $flvars=$flvars.'data-context="true"';
 
-        return '<iframe width="'.$width.'" height="'.$height.'" src="https://'.$url.'/embed/videoseries?list='.$vCode.'" frameborder="0" '.$fscr.'></iframe>';
+        return '<a data-flickr-embed="true" '.$flvars.'  href="https://www.flickr.com/photos/137485300@N04/28985161313/in/pool-flickrvideo/"><img src="https://c2.staticflickr.com/9/8391/28985161313_533eff3767_b.jpg" width="'.$width.'" height="'.$height.'"></a><script async src="https://embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>';
     }
 
     function vineVideo($vCode)
@@ -164,11 +162,9 @@ class plgContentcloudvideo extends JPlugin
 
         $width = $params->get('width', 432);
         $height = $params->get('height', 243);
-        $confidence = $params->get('confidence', 0);
         $fullscreen = $params->get('fullscreen', 1);
 
         #Logic
-        if($confidence) $url='www.youtube-nocookie.com'; else $url='www.youtube.com';
         if($fullscreen) $fscr=' allowfullscreen'; else $fscr='';
 
         return '<iframe width="'.$width.'" height="'.$height.'" src="https://'.$url.'/embed/videoseries?list='.$vCode.'" frameborder="0" '.$fscr.'></iframe>';
@@ -180,13 +176,24 @@ class plgContentcloudvideo extends JPlugin
 
         $width = $params->get('width', 432);
         $height = $params->get('height', 243);
-        $confidence = $params->get('confidence', 0);
-        $fullscreen = $params->get('fullscreen', 1);
+        $autoplay = $params->get('scautoplay', 0);
+        $hide_related = $params->get('scautoplay', 0);
+        $show_comments = $params->get('autoplay', 0);
+        $show_user = $params->get('autoplay', 1);
+        $show_reposts = $params->get('autoplay', 1);
+        $visual = $params->get('autoplay', 1);
+
+        auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true
 
         #Logic
-        if($confidence) $url='www.youtube-nocookie.com'; else $url='www.youtube.com';
-        if($fullscreen) $fscr=' allowfullscreen'; else $fscr='';
+        $scvars='';
+        if($autoplay) $scvars=$scvars.'auto_play=true&amp;'; else $scvars=$scvars.'auto_play=false&amp;';
+        if($hide_related) $scvars=$scvars.'hide_related=true&amp;'; else $scvars=$scvars.'hide_related=false&amp;';
+        if($show_comments) $scvars=$scvars.'show_comments=true&amp;'; else $scvars=$scvars.'show_comments=false&amp;';
+        if($show_user) $scvars=$scvars.'show_user=true&amp;'; else $scvars=$scvars.'show_user=false&amp;';
+        if($show_reposts) $scvars=$scvars.'show_reposts=true&amp;'; else $scvars=$scvars.'show_reposts=false&amp;';
+        if($visual) $scvars=$scvars.'visual=true&amp;'; else $scvars=$scvars.'visual=false&amp;';
 
-        return '<iframe width="'.$width.'" height="'.$height.'" src="https://'.$url.'/embed/videoseries?list='.$vCode.'" frameborder="0" '.$fscr.'></iframe>';
+        return '<iframe width="'.$width.'" height="'.$height.'" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/242660009&amp;'.$scvars.'></iframe>';
     }
 }
